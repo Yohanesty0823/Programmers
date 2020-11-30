57d99319684b53e3edddf8430d38271b6744c580
Administrator <Administrator@DESKTOP-I65DUKS>
2020-12-01 01:10:10 +0900

    완주하지 못한 선수 Comment 추가
    Pseudo Code 순서대로 Comment 변경

--- Programmers/src/com/yohanesty/algorithm/hash/PlayerOfUncompleted.java
+++ Programmers/src/com/yohanesty/algorithm/hash/PlayerOfUncompleted.java
@@ -18,21 +18,27 @@ public class PlayerOfUncompleted {
 }
 
 class Solution{
+	/*
+	 * #프로그래머스ㅡ해시ㅡ완주하지 못한 선수
+	 * 참가자와 완주자를 정렬하여, 
+	 * 완주자는 참가자보다 개수가 1이 적다는 조건으로 인해, 비교 반복문에서 리턴하지 않으면 마지막 참가자가 완주하지 못한 선수이다.
+	 * 
+	 * */
 	public String solution(String[] participant, String[] completion) {
+		// 1. 입력 값 배열을 정렬한다.
         // 참가자 정렬
         Arrays.sort(participant);
         // 완주자 정렬
         Arrays.sort(completion);
+        // 2. 완주자 길이만큼 반복, 참가자와 완주자를 비교하여 참가자와 완주자와 같지 않은 경우를 리턴한다.
         int i;
-        // 완주자 명수만큼 반복
         for(i=0; i<completion.length; i++){
         	// 참가자 중 완주자가 아닌 경우
             if(!participant[i].equals(completion[i]))
             	// 참가자 리턴
                 return participant[i];
         }
-        // 가장 마지막 참가자를 리턴(완주자는 참가자보다 개수가 1이 적다는 조건으로 인해, 비교 반복문에서 리턴하지 않으면 마지막 참가자가 완주하지 못한 선수이다.)
+        // 3. 반복문 종료 후, 마지막 남은 참가자를 리턴한다.
         return participant[i];
-        
     }
 }
